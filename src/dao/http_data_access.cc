6c47b8c086c469900b7b8ca0f0c2f17182eaaa55
David Morton <davidmortonmail@gmail.com>
2016-01-13 17:30:24 +0000

    Starting data access object

---
+++ src/dao/http_data_access.cc
@@ -0,0 +1,21 @@
+#include "dao/http_data_access.h"
+
+void HttpDao::Create()
+{
+
+}
+
+void HttpDao::Read()
+{
+
+}
+
+void HttpDao::Update()
+{
+
+}
+
+void HttpDao::Delete()
+{
+
+}
---
+++ src/dao/http_data_access.h
@@ -0,0 +1,12 @@
+#ifndef DAO_DATA_ACCESS_H
+#define DAO_DATA_ACCESS_H
+
+class HttpDao {
+public:
+	void Create();
+	void Read();
+	void Update();
+	void Delete();
+};
+
+#endif
