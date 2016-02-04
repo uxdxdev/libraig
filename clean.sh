bfec6b8cb08bb7609be03120b459eb0f1c360494
David Morton <davidmortonmail@gmail.com>
2016-02-04 20:23:11 +0000

    build for win10x86

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-rm -rf build cmake_install.cmake CMakeCache.txt CMakeFiles external libraig lib/ Makefile
+rm -rf CMakeCache.txt CMakeFiles
--- install_libsocket.sh
+++ install_libsocket.sh
@@ -1,3 +1,3 @@
-cd external
+mkdir external && cd external
 git clone https://damorton@bitbucket.org/damorton/libsocket.git && cd libsocket
 git checkout windows
