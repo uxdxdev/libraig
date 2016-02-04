04b2eb21c6ac67e57a70a86b3242def954aacf2c
David <davidmortonmail@gmail.com>
2016-02-04 10:05:20 +0000

    updated install script

--- install_libsocket.sh
+++ install_libsocket.sh
@@ -1,6 +1,3 @@
 cd external
 git clone https://damorton@bitbucket.org/damorton/libsocket.git && cd libsocket
 git checkout windows
-echo "Navigate to the cocos2d/external/libraig directory and build the project for specific plaform."
-echo "- For Windows open the .sln file and build the .dll file"
-echo "- For Linux run 'make'"
