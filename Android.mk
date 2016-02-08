c142109bb1a589e8ba0566b2e76b81bb609de84e
David Morton <davidmortonmail@gmail.com>
2016-02-08 21:23:49 +0000

    updated android make file

--- Android.mk
+++ Android.mk
@@ -8,9 +8,9 @@ LOCAL_MODULE_FILENAME := libsocket
 
 LOCAL_SRC_FILES :=	$(LOCAL_PATH)/external/libsocket/src/socket.c
 
-LOCAL_EXPORT_C_INCLUDES :=	$(LOCAL_PATH)/external/libsocket/include
+LOCAL_EXPORT_C_INCLUDES :=	$(LOCAL_PATH)/external											
 
-LOCAL_C_INCLUDES :=	$(LOCAL_PATH)/external/libsocket/include					 
+LOCAL_C_INCLUDES :=	$(LOCAL_PATH)/external/libsocket					 
 
 include $(BUILD_SHARED_LIBRARY)
 
